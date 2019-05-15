9d3a6be5c209a86f81f6980b2513fa436d38851b
ParisotRomain <parisot.romain54@gmail.com>
2019-05-15 13:12:46 +0200

    correctly changes for several games

--- src/main/java/hello/PenduController.java
+++ src/main/java/hello/PenduController.java
@@ -1,9 +1,11 @@
-package Pendu;
+package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import Pendu.Game;
 
 @Controller
 public class PenduController {
